957d3a955396330aa9da339475957ef8d2460c4a
Sohini Ball <ballsohini939@gmail.com>
2026-08-06 23:08:41 +0530

    Add JavaScript conditions practice

---
+++ 04_conditions.js
@@ -0,0 +1,12 @@
+let marks = 97;
+if (marks >= 90) {
+    console.log("Grade: A+");
+} else if (marks >= 80) {
+    console.log("Grade: A");
+} else if (marks >= 70) {
+    console.log("Grade: B");
+} else if (marks >= 60) {
+    console.log("Grade: C");
+} else {
+    console.log("Grade: F");
+}
