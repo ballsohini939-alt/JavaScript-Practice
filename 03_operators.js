e4bab6ddb4c854bdf5ace98aec7ff02e659e118c
Sohini Ball <ballsohini939@gmail.com>
2026-08-06 23:05:29 +0530

    Add JavaScript operators practice

---
+++ 03_operators.js
@@ -0,0 +1,12 @@
+let a = 90;
+let b = 6;
+console.log("Addition:", a + b);
+console.log("Subtraction:", a - b);
+console.log("Multiplication:", a * b);
+console.log("Division:", a / b);
+console.log("Remainder:", a % b);
+
+console.log("a > b:", a > b);
+console.log("a < b:", a < b);
+console.log("a == b:", a == b);
+console.log("a === b:", a === b);
