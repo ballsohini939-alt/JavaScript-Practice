8834798738d85db37f48b9328e5a2461ec190d9f
Sohini Ball <ballsohini939@gmail.com>
2026-08-08 17:57:43 +0530

    Add JavaScript objects practice

---
+++ 09_objects.js
@@ -0,0 +1,28 @@
+// 1. Creating an object
+let student = {
+    name: "Sohini",
+    age: 18,
+    course: "CSE"
+};
+console.log(student);
+
+
+// 2. Accessing object properties
+console.log("Name:", student.name);
+console.log("Age:", student.age);
+console.log("Course:", student.course);
+
+
+// 3. Changing an object property
+student.age = 19;
+console.log("After changing age:");
+console.log(student);
+
+
+// 4. Adding a new property
+student.city = "Asansol";
+console.log("After adding city:");
+console.log(student);
+student.college = "Asansol Engineering College";
+console.log("After adding college:");
+console.log(student);
